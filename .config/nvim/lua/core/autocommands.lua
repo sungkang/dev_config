bea411a67ff1ff2be5acffe4bcfbf1cd46e66a9b
Sung Kang <sung@sketchdev.io>
2022-12-31 03:24:19 -0600

    move the comments outside the multi-line comment

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -7,16 +7,17 @@ M.config = function()
       autocmd BufRead * autocmd BufWinEnter * ++once normal! zx zR
     augroup END
 
-    -- When opening the help buffer, upon open, set the buffer to the right most vertical split.
-    -- The winwidth() check is to determine whether a help buffer is already open, if so, do not issue the wincmd.
-    -- The reason for this is because telescope.nvim issues a close window command when closing the prompt buffer and when
-    -- the BufEnter event is triggered which then tries to issue the wincmd, you will get an error stating that a split
-    -- cannot be issued while a window is closing.
     augroup _help
       autocmd!
       autocmd BufEnter *.txt if &buftype == 'help' && winwidth(0) == &columns | wincmd L | endif
     augroup END
   ]])
+    -- Notes: _help
+    -- When opening the help buffer, upon open, set the buffer to the right most vertical split.
+    -- The winwidth() check is to determine whether a help buffer is already open, if so, do not issue the wincmd.
+    -- The reason for this is because telescope.nvim issues a close window command when closing the prompt buffer and when
+    -- the BufEnter event is triggered which then tries to issue the wincmd, you will get an error stating that a split
+    -- cannot be issued while a window is closing.
 end
 
 return M
