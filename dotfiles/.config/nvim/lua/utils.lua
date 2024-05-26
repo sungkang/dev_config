aeaac3d4caf066a019929cf72a4fc95a6d721e36
Sung Kang <sk5156@gmail.com>
2024-05-25 23:33:04 -0500

    add lazygit config and function for edit

--- dotfiles/.config/nvim/lua/utils.lua
+++ dotfiles/.config/nvim/lua/utils.lua
@@ -84,4 +84,10 @@ function _G.P(...)
   print(unpack(objects))
 end
 
+function M.lazygit_open_file(filename, line_number)
+  line_number = tonumber(line_number) or 1
+  vim.api.nvim_win_close(0, true)
+  vim.api.nvim_command('edit +' .. line_number .. " " .. filename)
+end
+
 return M
