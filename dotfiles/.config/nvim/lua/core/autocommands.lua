9885c5dcdfdd8cedabcb0342ada952737bb643e6
Sung Kang <sk5156@gmail.com>
2025-09-04 13:55:24 -0500

    add autocommand to fix folding issue when opening buffer for first time

--- dotfiles/.config/nvim/lua/core/autocommands.lua
+++ dotfiles/.config/nvim/lua/core/autocommands.lua
@@ -1,11 +1,19 @@
 M = {}
 
 M.config = function()
-  vim.api.nvim_create_autocmd("BufWinEnter", {
-    pattern = "*",
-    command = "normal! zR",
-    once = true,
-  })
+  local option = vim.opt
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        vim.defer_fn(function()
+            option.foldmethod = "expr"
+            option.foldexpr = "nvim_treesitter#foldexpr()"
+            option.foldcolumn = "1"
+            option.foldnestmax = 3
+            option.foldlevel = 99
+            option.foldlevelstart = 99
+        end, 100)
+    end,
+})
 
   vim.cmd([[
     augroup _help
