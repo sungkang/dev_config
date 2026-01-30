7787e9e688913098ad7176f8feb5e0e21d737c49
Sung Kang <sk5156@gmail.com>
2026-01-30 15:05:29 -0600

    update

--- dotfiles/.local/bin/env.fish
+++
@@ -1,4 +0,0 @@
-if not contains "$HOME/.local/share/../bin" $PATH
-    # Prepending path in case a system-installed binary needs to be overridden
-    set -x PATH "$HOME/.local/share/../bin" $PATH
-end
