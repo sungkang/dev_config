58290e3d717ba88a5e61393416aeec4d53848270
Sung Kang <sk5156@gmail.com>
2026-04-07 09:19:21 -0500

    change foldexpr when buffer attaches an lsp

--- dotfiles/common/nvim/.config/nvim/lua/core/autocommands.lua
+++ dotfiles/common/nvim/.config/nvim/lua/core/autocommands.lua
@@ -23,6 +23,16 @@ M.config = function()
     end,
   })
 
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client and client.supports_method("textDocument/foldingRange") then
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
+      end
+    end
+  })
+
   vim.cmd([[
     augroup _help
       autocmd!
